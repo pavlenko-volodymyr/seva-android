1c0f828d94ab6f5587545035e42f638a9d19fabd
Oleg Melenevsky <oleg_me@mail.ru>
2013-09-12 23:23:15 +0300

    Somehow app now makes http connection and displays parsed JSON data

--- src/src/com/mela/seva/MainActivity.java
+++ src/src/com/mela/seva/MainActivity.java
@@ -1,17 +1,33 @@
 package com.mela.seva;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+@SuppressLint("NewApi")
+public class MainActivity extends Activity implements OnClickListener {
 	TextView textView;
+	Button button;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +35,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		textView = (TextView) findViewById(R.id.textView);
-		
-		String users = "{\"meta\": {\"limit\": 20, \"next\": null, \"offset\": 0, \"previous\": null, \"total_count\": 1}, \"objects\": [{\"avg_level\": 4.0, \"bio\": \"the best\", \"email\": \"mel@mail.com\", \"evaluations\": [{\"created\": \"2013-09-11T19:53:27.878332\", \"is_favorite\": true, \"level\": 4, \"logo\": \"\", \"technology\": \"PostgreSQL\", \"technology_slug\": \"postgresq\", \"updated\": \"2013-09-11T19:53:27.878391\"}], \"favorites\": [\"PostgreSQL\"], \"full_name\": \"\", \"id\": 1, \"is_active\": true, \"last_login\": \"2013-09-11T19:47:06.625023\", \"resource_uri\": \"/api/v1/users/mel/\", \"username\": \"mel\"}]}";
-		String technology = "{\"meta\": {\"limit\": 20, \"next\": null, \"offset\": 0, \"previous\": null, \"total_count\": 2}, \"objects\": [{\"avg\": null, \"resource_uri\": \"/api/v1/technology/1/\", \"slug\": \"django\", \"title\": \"Django\"}, {\"avg\": 4.0, \"resource_uri\": \"/api/v1/technology/2/\", \"slug\": \"postgresq\", \"title\": \"PostgreSQL\"}]}";
-		
-		try {
-			JSONObject jUsers = new JSONObject(users);
-			JSONObject jTechnologies= new JSONObject(technology);
-			
-			JSONArray objects = jTechnologies.getJSONArray("objects");
-			
-			// TODO nice representation
-			textView.setText(objects.getJSONObject(0).getString("slug"));
-			
-		} catch (JSONException e) {
-			e.printStackTrace();
-			Toast.makeText(this, "Bad data recieed from server", Toast.LENGTH_LONG).show();
-		}
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(this);
 	}
 
 	@Override
@@ -44,5 +45,70 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	@SuppressLint("NewApi")
+	private void getdata(){
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.
+                    ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy); 
+            URL url = new URL("http://seva.djangostars.com/api/v1/technology/?format=json");
+            HttpURLConnection con = (HttpURLConnection) url
+              .openConnection();
+            readStream(con.getInputStream());
+            } catch (Exception e) {
+            e.printStackTrace();
+          }
+
+    }
+  private void readStream(InputStream in) {
+    BufferedReader reader = null;
+    try {
+      reader = new BufferedReader(new InputStreamReader(in));
+      StringBuilder builder = new StringBuilder();
+      String line = "";
+      while ((line = reader.readLine()) != null) {
+        builder.append(line);
+      }
+      
+      try {
+//			JSONObject jUsers = new JSONObject(users);
+			JSONObject jTechnologies= new JSONObject(builder.toString());
+			
+			JSONArray objects = jTechnologies.getJSONArray("objects");
+		
+		// TODO nice representation
+		StringBuilder result = new StringBuilder();
+		for (int i=0;i<objects.length();i++) {
+			result.append(objects.getJSONObject(i).getString("title"))
+			.append(" - ")
+			.append(objects.getJSONObject(i).getString("avg"))
+			.append('\n');
+		}
+		textView.setText(result.toString());
+      } catch (JSONException e) {
+			e.printStackTrace();
+			Toast.makeText(this, "Bad data recieed from server", Toast.LENGTH_LONG).show();
+		}
+		
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (reader != null) {
+        try {
+          reader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+          }
+      }
+    }
+  }
+
+@Override
+public void onClick(View arg0) {
+	getdata();
+	
+} 
 
 }
